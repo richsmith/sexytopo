323d5dc85579dd534b560f19602c5afe0987f74d
Rich Smith <rls@hwyl.org>
2016-09-23 22:45:38 +0100

    Parse full sketch block

--- app/src/main/java/org/hwyl/sexytopo/control/io/thirdparty/xvi/XviImporter.java
+++ app/src/main/java/org/hwyl/sexytopo/control/io/thirdparty/xvi/XviImporter.java
@@ -2,15 +2,27 @@ package org.hwyl.sexytopo.control.io.thirdparty.xvi;
 
 import org.hwyl.sexytopo.control.io.basic.Loader;
 import org.hwyl.sexytopo.control.io.translation.Importer;
+import org.hwyl.sexytopo.model.graph.Coord2D;
+import org.hwyl.sexytopo.model.sketch.Colour;
+import org.hwyl.sexytopo.model.sketch.PathDetail;
 import org.hwyl.sexytopo.model.sketch.Sketch;
 import org.hwyl.sexytopo.model.survey.Survey;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.hwyl.sexytopo.control.io.thirdparty.xvi.XviConstants.SKETCHLINE_COMMAND;
 
 
 public class XviImporter implements Importer {
 
-    public Survey toSurvey(File file) {
+    public Survey toSurvey(File file) throws Exception {
         Survey survey = new Survey(file.getName());
         String contents = Loader.slurpFile(file);
         Sketch sketch = getSketch(contents);
@@ -18,14 +30,78 @@ public class XviImporter implements Importer {
         return survey;
     }
 
-    public static Sketch getSketch(String contents) {
+    public static Sketch getSketch(String contents) throws Exception {
         Sketch sketch = new Sketch();
+
+        Set<PathDetail> pathDetails = parseSketchlineBlock(contents);
+        sketch.setPathDetails(pathDetails);
+
         return sketch;
     }
+
 
     public boolean canHandleFile(File file) {
         return file.getName().endsWith("xvi");
     }
+
+    public static Set<PathDetail> parseSketchlineBlock(String contents) throws Exception {
+        String sketchBlock = getBlockContents(contents, SKETCHLINE_COMMAND);
+        List<String> entries = parseBlockEntries(sketchBlock);
+        Set<PathDetail> pathDetails = new HashSet<>();
+        for (String entry : entries) {
+            PathDetail pathDetail = parseSketchEntry(entry);
+            pathDetails.add(pathDetail);
+        }
+        return pathDetails;
+    }
+
+    public static List<String> parseBlockEntries(String content) {
+        List<String> entries = new ArrayList<>();
+        Pattern pattern = Pattern.compile("{(.*)}");
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()) {
+            entries.add(matcher.group());
+        }
+        return entries;
+    }
+
+
+    public static PathDetail parseSketchEntry(String entry) throws IllegalArgumentException {
+        List<String> tokens = Arrays.asList(entry.split(" "));
+
+        if (tokens.size() <= 1) {
+            throw new IllegalArgumentException("Incomplete token? {" + entry + "}");
+        }
+
+        String first = tokens.get(0);
+        if (first.equals("connect")) {
+            throw new IllegalArgumentException("Not sure what to do with token {" + entry + "}");
+        }
+
+        if (tokens.size() % 2 != 1) {
+            throw new IllegalArgumentException(
+                    "There was an odd number of data points in the token (excluding first item): "
+                            + tokens.size());
+        }
+
+        Colour colour = Colour.valueOf(first.toUpperCase());
+
+        List<Coord2D> points = new ArrayList<>(tokens.size() - 1);
+        for (int i = 1; i < tokens.size();) {
+            double x = Double.parseDouble(tokens.get(i++));
+            double y = Double.parseDouble(tokens.get(i++));
+            Coord2D coord2D = new Coord2D(x, y);
+            points.add(coord2D);
+        }
+
+        PathDetail pathDetail = new PathDetail(points.get(0), colour);
+        for (Coord2D point : points.subList(1, points.size())) {
+            pathDetail.lineTo(point);
+        }
+
+        return pathDetail;
+    }
+
 
     public static String getBlockContents(String text, String command) throws Exception {
 
--- app/src/test/java/org/hwyl/sexytopo/control/io/thirdparty/xvi/XviImporterTest.java
+++ app/src/test/java/org/hwyl/sexytopo/control/io/thirdparty/xvi/XviImporterTest.java
@@ -2,7 +2,12 @@ package org.hwyl.sexytopo.control.io.thirdparty.xvi;
 
 import junit.framework.Assert;
 
+import org.hwyl.sexytopo.model.graph.Coord2D;
+import org.hwyl.sexytopo.model.sketch.Colour;
+import org.hwyl.sexytopo.model.sketch.PathDetail;
 import org.junit.Test;
+
+import java.util.List;
 
 
 public class XviImporterTest {
@@ -33,5 +38,78 @@ public class XviImporterTest {
         String simpleText = "set Command {{foo}}";
         String contents = XviImporter.getBlockContents(simpleText, "set Command");
         Assert.assertEquals("{foo}", contents);
+    }
+
+    @Test(expected=Exception.class)
+    public void testBadlyNestedBlockContentsRaiseError() throws Exception {
+        String simpleText = "set Command {{foo}";
+        String contents = XviImporter.getBlockContents(simpleText, "set Command");
+        Assert.assertEquals("{foo}", contents);
+    }
+
+    @Test(expected=Exception.class)
+    public void testNoCommandMatchRaiseError() throws Exception {
+        String simpleText = "set Commandx {foo}";
+        String contents = XviImporter.getBlockContents(simpleText, "set Command");
+        Assert.assertEquals("{foo}", contents);
+    }
+
+
+    @Test(expected=Exception.class)
+    public void testParseBlockEntriesSingle() throws Exception {
+        String simpleText = "{foo}";
+        List<String> contents = XviImporter.parseBlockEntries(simpleText);
+        Assert.assertEquals("foo", contents.get(0));
+    }
+
+    @Test(expected=Exception.class)
+    public void testParseBlockEntriesMultiple() throws Exception {
+        String simpleText = "{foo}{bar}";
+        List<String> contents = XviImporter.parseBlockEntries(simpleText);
+        Assert.assertEquals("foo", contents.get(0));
+        Assert.assertEquals("bar", contents.get(1));
+    }
+
+    @Test(expected=Exception.class)
+    public void testParseBlockEntriesMultipleWithWhitespace() throws Exception {
+        String simpleText = "\t{foo}\n   {bar}";
+        List<String> contents = XviImporter.parseBlockEntries(simpleText);
+        Assert.assertEquals("foo", contents.get(0));
+        Assert.assertEquals("bar", contents.get(1));
+    }
+
+    @Test
+    public void testParseSketchEntryGetsRightColour() throws Exception {
+        String simpleText = "red 0 0";
+        PathDetail pathDetail = XviImporter.parseSketchEntry(simpleText);
+        Assert.assertEquals(Colour.RED, pathDetail.getColour());
+    }
+
+    @Test
+    public void testParseSketchEntryParsesFirstEntry() throws Exception {
+        String simpleText = "red 0 0";
+        PathDetail pathDetail = XviImporter.parseSketchEntry(simpleText);
+        Assert.assertEquals(Coord2D.ORIGIN, pathDetail.getPath().get(0));
+        Assert.assertEquals(1, pathDetail.getPath().size());
+    }
+
+    @Test
+    public void testParseSketchEntryParsesSecondEntry() throws Exception {
+        String simpleText = "blue 0 0 1 0";
+        PathDetail pathDetail = XviImporter.parseSketchEntry(simpleText);
+        Assert.assertEquals(new Coord2D(1, 0), pathDetail.getPath().get(1));
+        Assert.assertEquals(2, pathDetail.getPath().size());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testParseSketchEntryFailsForUnevenData() throws Exception {
+        String simpleText = "red 0 0 1";
+        PathDetail pathDetail = XviImporter.parseSketchEntry(simpleText);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testParseSketchEntryFailsForUnknownFirstCommand() throws Exception {
+        String simpleText = "connect 0 0";
+        PathDetail pathDetail = XviImporter.parseSketchEntry(simpleText);
     }
 }
