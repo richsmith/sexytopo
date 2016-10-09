719aba645982b6a15181695e109542213ee6d772
Rich Smith <rls@hwyl.org>
2016-10-09 11:12:41 +0100

    First attempt at a Therion thconfig exporter

---
+++ app/src/main/java/org/hwyl/sexytopo/control/io/thirdparty/therion/ThconfigExporter.java
@@ -0,0 +1,25 @@
+package org.hwyl.sexytopo.control.io.thirdparty.therion;
+
+import org.hwyl.sexytopo.control.util.TextTools;
+import org.hwyl.sexytopo.model.survey.Survey;
+
+import java.util.Arrays;
+
+
+public class ThconfigExporter {
+
+    public String export(Survey survey) {
+
+        String name = survey.getName();
+
+        String[] lines = new String[] {
+            TherionExporter.getEncodingText(),
+            "source " + name + ".th",
+            "export map -proj plan -layout local -o " + name + "-plan.pdf",
+            "export map -proj extended -layout local -o " + name + "-ee.pdf"
+        };
+
+        return TextTools.join(Arrays.asList(lines), "\n\n");
+    }
+
+}
