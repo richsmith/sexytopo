fa9f2c3f1d2cc2f3ae847bbce85375e311a242d6
Rich Smith <rls@hwyl.org>
2016-10-21 22:02:06 +0100

    Update version for latest release

--- app/src/main/java/org/hwyl/sexytopo/SexyTopo.java
+++ app/src/main/java/org/hwyl/sexytopo/SexyTopo.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 public class SexyTopo {
 
     public static final String APP_NAME = "SexyTopo";
-    public static final String VERSION = "1.0.9";
+    public static final String VERSION = "1.0.11";
 
     public static final String TAG = APP_NAME; // for writing debug log
 
