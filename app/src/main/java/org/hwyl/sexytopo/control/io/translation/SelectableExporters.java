a4ec1fed219e6561c1520fd2290781bf0db3b3db
Rich Smith <rls@hwyl.org>
2017-03-02 20:17:04 +0000

    Restore Compass as a selectable exporter (not sure how that got missed out)

--- app/src/main/java/org/hwyl/sexytopo/control/io/translation/SelectableExporters.java
+++ app/src/main/java/org/hwyl/sexytopo/control/io/translation/SelectableExporters.java
@@ -1,5 +1,6 @@
 package org.hwyl.sexytopo.control.io.translation;
 
+import org.hwyl.sexytopo.control.io.thirdparty.compass.CompassExporter;
 import org.hwyl.sexytopo.control.io.thirdparty.pockettopo.PocketTopoTxtExporter;
 import org.hwyl.sexytopo.control.io.thirdparty.survex.SurvexExporter;
 import org.hwyl.sexytopo.control.io.thirdparty.therion.TherionExporter;
@@ -13,7 +14,8 @@ public class SelectableExporters {
     public static final List<? extends Exporter> EXPORTERS = Arrays.asList(
         new TherionExporter(),
         new SurvexExporter(),
-        new PocketTopoTxtExporter()
+        new PocketTopoTxtExporter(),
+        new CompassExporter()
     );
 
 }
