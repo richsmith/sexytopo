b430e5730e5473543b2b9dceb68d510c1bd0bf6b
Rich Smith <rls@hwyl.org>
2018-02-08 19:18:54 +0000

    Work for future debug logging

--- app/src/main/java/org/hwyl/sexytopo/control/Log.java
+++ app/src/main/java/org/hwyl/sexytopo/control/Log.java
@@ -24,6 +24,11 @@ public class Log {
     private static final int MAX_DEVICE_LOG_SIZE = 100;
     private static Queue<Message> deviceLog = new LinkedList<>();
 
+    private static final int MAX_SYSTEM_LOG_SIZE = 100;
+    private static Queue<Message> systemLog = new LinkedList<>();
+
+
+
     public static void setContext(Context context) {
         Log.context = context;
     }
@@ -38,12 +43,18 @@ public class Log {
         android.util.Log.i(SexyTopo.TAG, message);
     }
 
-    public static void d(String message) {
-        android.util.Log.d(SexyTopo.TAG, message);
+    public static synchronized void systemLog(String message, boolean isError) {
+
+        if (deviceLog.size() >= MAX_SYSTEM_LOG_SIZE) {
+            deviceLog.remove();
+        }
+        deviceLog.add(new Message(message));
+        broadcast(SexyTopo.DEVICE_LOG_UPDATED_EVENT);
+        android.util.Log.i(SexyTopo.TAG, message);
     }
 
-    @Deprecated
-    public static void d(String tag, String message) {
+    public static void d(String message) {
+
         android.util.Log.d(SexyTopo.TAG, message);
     }
 
@@ -51,10 +62,6 @@ public class Log {
         android.util.Log.e(SexyTopo.TAG, message);
     }
 
-    @Deprecated
-    public static void e(String tag, String message) {
-        android.util.Log.e(SexyTopo.TAG, message);
-    }
 
     public static void broadcast(String event) {
         Intent intent = new Intent(event);
@@ -67,13 +74,17 @@ public class Log {
     }
 
     public static synchronized List<Message> getDeviceLog() {
-
-
         try {
             return new ArrayList<>(deviceLog);
-            //return (List)deviceLog;
         } catch (Exception e) {
-            int ignore = 0;
+            return new ArrayList<>();
+        }
+    }
+
+    public static synchronized List<Message> getSystemLog() {
+        try {
+            return new ArrayList<>(deviceLog);
+        } catch (Exception e) {
             return new ArrayList<>();
         }
     }
@@ -87,9 +98,16 @@ public class Log {
     public final static class Message {
         private final String text;
         private final Date timestamp;
-        public Message(String message) {
+        private final boolean isError;
+
+        public Message(String message, boolean isError) {
             this.text = message;
             this.timestamp = new Date();
+            this.isError = isError;
+        }
+
+        public Message(String message) {
+            this(message, false);
         }
 
         public String getText() {
