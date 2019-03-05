b8b06147e7d1b757a171873617f64bab5da81216
Rich Smith <rls@hwyl.org>
2019-03-05 20:20:59 +0000

    Make Settings activity a SexyTopo activity for menubar etc.

--- app/src/main/java/org/hwyl/sexytopo/control/activity/SettingsActivity.java
+++ app/src/main/java/org/hwyl/sexytopo/control/activity/SettingsActivity.java
@@ -1,17 +1,15 @@
 package org.hwyl.sexytopo.control.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 import org.hwyl.sexytopo.R;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends SexyTopoActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
@@ -21,8 +19,6 @@ public class SettingsActivity extends Activity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
-            // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.general_preferences);
         }
 
