54ba56e5a6e2ecb62e0217048b11c06e9997f5d3
Rich Smith <rls@hwyl.org>
2019-06-04 22:30:49 +0100

    Improve Stats view with more stats

--- app/src/main/java/org/hwyl/sexytopo/control/activity/SurveyActivity.java
+++ app/src/main/java/org/hwyl/sexytopo/control/activity/SurveyActivity.java
@@ -1,7 +1,6 @@
 package org.hwyl.sexytopo.control.activity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import org.hwyl.sexytopo.R;
@@ -9,18 +8,12 @@ import org.hwyl.sexytopo.control.util.SurveyStats;
 import org.hwyl.sexytopo.control.util.TextTools;
 import org.hwyl.sexytopo.model.survey.Survey;
 
-public class SurveyActivity extends SexyTopoActivity implements View.OnClickListener {
+public class SurveyActivity extends SexyTopoActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-
-        /*
-        View saveButton = findViewById(R.id.buttonSaveSurvey);
-        saveButton.setOnClickListener(this);
-        */
     }
 
     @Override
@@ -30,26 +23,9 @@ public class SurveyActivity extends SexyTopoActivity implements View.OnClickList
     }
 
 
-    @Override
-    public void onClick(View view) {
-        /*
-        switch (view.getId()) {
-               case R.id.buttonSaveSurvey:
-                try {
-                    Saver.save(this, getSurvey());
-                } catch (Exception e) {
-                    showSimpleToast("Error saving survey");
-                    Log.e("Error saving survey: " + e);
-                }
-        }*/
-    }
-
     private void updateStats() {
 
         Survey survey = getSurvey();
-
-        //TextView nameField = (TextView)(findViewById(R.id.survey_name));
-        //nameField.setText(survey.getName());
 
         double length = SurveyStats.calcTotalLength(survey);
         setStatsField(R.id.statsFieldLength, TextTools.formatTo2dpWithComma(length));
@@ -57,15 +33,18 @@ public class SurveyActivity extends SexyTopoActivity implements View.OnClickList
         setStatsField(R.id.statsFieldDepth, TextTools.formatTo2dpWithComma(heightRange));
         int numberOfStations = SurveyStats.calcNumberStations(survey);
         setStatsField(R.id.statsFieldNumberStations, TextTools.formatWithComma(numberOfStations));
+        int numberOfLegs = SurveyStats.calcNumberSubFullLegs(survey.getOrigin());
+        setStatsField(R.id.statsFieldNumberLegs, TextTools.formatWithComma(numberOfLegs));
+        int numberOfSplays = SurveyStats.calcNumberSubSplays(survey.getOrigin());
+        setStatsField(R.id.statsFieldNumberSplays, TextTools.formatWithComma(numberOfSplays));
         double shortestLeg = SurveyStats.calcShortestLeg(survey);
         setStatsField(R.id.statsFieldShortestLeg, TextTools.formatTo2dpWithComma(shortestLeg));
         double longestLeg = SurveyStats.calcLongestLeg(survey);
         setStatsField(R.id.statsFieldLongestLeg, TextTools.formatTo2dpWithComma(longestLeg));
-
     }
 
     private void setStatsField(int id, String text) {
-        TextView textView = (TextView)findViewById(id);
+        TextView textView = findViewById(id);
         textView.setText(text);
     }
 
