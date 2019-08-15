7ed838aa8bcabac095f70dd1ab0ee5d6bc95b426
Rich Smith <rls@hwyl.org>
2019-08-15 19:17:09 +0100

    Disable PocketTopo .txt export

--- app/src/main/java/org/hwyl/sexytopo/control/io/translation/SelectableExporters.java
+++ app/src/main/java/org/hwyl/sexytopo/control/io/translation/SelectableExporters.java
@@ -1,7 +1,6 @@
 package org.hwyl.sexytopo.control.io.translation;
 
 import org.hwyl.sexytopo.control.io.thirdparty.compass.CompassExporter;
-import org.hwyl.sexytopo.control.io.thirdparty.pockettopo.PocketTopoTxtExporter;
 import org.hwyl.sexytopo.control.io.thirdparty.survex.SurvexExporter;
 import org.hwyl.sexytopo.control.io.thirdparty.svg.SvgExporter;
 import org.hwyl.sexytopo.control.io.thirdparty.therion.TherionExporter;
@@ -15,7 +14,7 @@ public class SelectableExporters {
     public static final List<? extends Exporter> EXPORTERS = Arrays.asList(
         new TherionExporter(),
         new SurvexExporter(),
-        new PocketTopoTxtExporter(),
+        // new PocketTopoTxtExporter(), <- disabled due to several issues
         new CompassExporter(),
         new SvgExporter()
     );
