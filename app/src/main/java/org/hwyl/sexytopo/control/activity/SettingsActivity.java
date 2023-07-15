981387590d3862afcd3d2b7d955b756a909cca47
Rich Smith <rls@hwyl.org>
2023-07-15 14:23:47 +0100

    Update deprecated stuff and libraries

--- app/src/main/java/org/hwyl/sexytopo/control/activity/SettingsActivity.java
+++ app/src/main/java/org/hwyl/sexytopo/control/activity/SettingsActivity.java
@@ -1,7 +1,8 @@
 package org.hwyl.sexytopo.control.activity;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+
+import androidx.preference.PreferenceFragmentCompat;
 
 import org.hwyl.sexytopo.R;
 
@@ -10,17 +11,16 @@ public class SettingsActivity extends SexyTopoActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        setContentView(R.layout.activity_settings);
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.settings_container, new SettingsFragment())
+            .commit();
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+        public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
             addPreferencesFromResource(R.xml.general_preferences);
         }
-
     }
 }
