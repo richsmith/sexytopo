79d0d672dc3215c161dea2c140b2ef7e48ea9743
Rich Smith <rls@hwyl.org>
2025-12-14 19:20:40 +0000

    I think we needed this

--- app/src/main/java/org/hwyl/sexytopo/model/sketch/AutoScalableDetail.java
+++ app/src/main/java/org/hwyl/sexytopo/model/sketch/AutoScalableDetail.java
@@ -1,7 +1,10 @@
 package org.hwyl.sexytopo.model.sketch;
 
+import org.hwyl.sexytopo.model.graph.Coord2D;
+
 public interface AutoScalableDetail {
 
+    Coord2D getPosition();
     float getSize();
 
 }
