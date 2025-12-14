6e2b97322e151e1d1ef048cf5d38486a4e847887
Rich Smith <rls@hwyl.org>
2025-12-14 19:20:06 +0000

    Tidier text translation

--- app/src/main/java/org/hwyl/sexytopo/control/io/thirdparty/xvi/TextDetailTranslater.java
+++ app/src/main/java/org/hwyl/sexytopo/control/io/thirdparty/xvi/TextDetailTranslater.java
@@ -6,7 +6,9 @@ import org.hwyl.sexytopo.model.sketch.PathDetail;
 import org.hwyl.sexytopo.model.sketch.TextDetail;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TextDetailTranslater extends SketchDetailTranslater<TextDetail> {
     private static final float CHAR_HEIGHT = 4.0f;
@@ -17,95 +19,70 @@ public class TextDetailTranslater extends SketchDetailTranslater<TextDetail> {
     // More art than science ;)
     private static final float SCALE_FACTOR = 0.15f;
 
+    /* ********** Glyph data copied from TopoDroid - thanks, Marco! ********** */
+    // Each character maps to an array of line segment coordinates [x1, y1, x2, y2, ...]
+    // Coordinates are in a 4-unit high space
+    private static final Map<Character, int[]> GLYPH_DATA = new HashMap<>();
 
-    /* ********** copied from TopoDroid - thanks, Marco! ********** */
-    static final private int[] CHAR_ANY = {2, 2, 1, 4, 1, 4, 0, 2, 0, 2, 1, 0, 1, 0, 2, 2};
-    static final private int[] CHAR_SPACE = {};
-    static final private int[] CHAR_UNDERSCORE = {0, 0, 2, 0};
-    static final private int[] CHAR_PLUS = {1, 1, 1, 3, 0, 2, 2, 2};
-    static final private int[] CHAR_MINUS = {0, 2, 2, 2};
-    static final private int[] CHAR_QUESTION = {1, 0, 1, 2, 0, 3, 0, 4, 0, 4, 2, 4, 1, 2, 2, 3, 2, 3, 2, 4};
-    static final private int[] CHAR_SLASH = {0, 0, 2, 4};
-    static final private int[] CHAR_LESS = {0, 2, 2, 3, 0, 2, 2, 1};
-    static final private int[] CHAR_MORE = {0, 3, 2, 2, 0, 1, 2, 2};
+    static {
+        // Special characters
+        GLYPH_DATA.put(' ', new int[] {});  // Space (empty)
+        GLYPH_DATA.put('_', new int[] {0, 0, 2, 0});
+        GLYPH_DATA.put('+', new int[] {1, 1, 1, 3, 0, 2, 2, 2});
+        GLYPH_DATA.put('-', new int[] {0, 2, 2, 2});
+        GLYPH_DATA.put('?', new int[] {1, 0, 1, 2, 0, 3, 0, 4, 0, 4, 2, 4, 1, 2, 2, 3, 2, 3, 2, 4});
+        GLYPH_DATA.put('/', new int[] {0, 0, 2, 4});
+        GLYPH_DATA.put('<', new int[] {0, 2, 2, 3, 0, 2, 2, 1});
+        GLYPH_DATA.put('>', new int[] {0, 3, 2, 2, 0, 1, 2, 2});
 
-    /*static final CHAR_TO_GLYPH = {
-    }*/
+        // Letters A-Z
+        GLYPH_DATA.put('A', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 2, 4, 2, 0, 0, 2, 2, 2});
+        GLYPH_DATA.put('B', new int[] {0, 0, 0, 4, 0, 4, 2, 3, 0, 2, 2, 2, 0, 0, 2, 0, 2, 3, 2, 0});
+        GLYPH_DATA.put('C', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0});
+        GLYPH_DATA.put('D', new int[] {0, 0, 0, 4, 0, 4, 2, 3, 2, 3, 2, 0, 0, 0, 2, 0});
+        GLYPH_DATA.put('E', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0});
+        GLYPH_DATA.put('F', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2});
+        GLYPH_DATA.put('G', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0, 2, 0, 2, 2, 1, 2, 2, 2});
+        GLYPH_DATA.put('H', new int[] {0, 0, 0, 4, 0, 2, 2, 2, 2, 0, 2, 4});
+        GLYPH_DATA.put('I', new int[] {0, 0, 0, 4});
+        GLYPH_DATA.put('J', new int[] {0, 0, 1, 0, 1, 0, 1, 4, 0, 4, 2, 4});
+        GLYPH_DATA.put('K', new int[] {0, 0, 0, 4, 0, 2, 2, 3, 0, 2, 2, 0});
+        GLYPH_DATA.put('L', new int[] {0, 0, 0, 4, 0, 0, 2, 0});
+        GLYPH_DATA.put('M', new int[] {0, 0, 0, 4, 0, 4, 1, 2, 1, 2, 2, 4, 2, 4, 2, 0});
+        GLYPH_DATA.put('N', new int[] {0, 0, 0, 4, 0, 4, 2, 0, 2, 4, 2, 0});
+        GLYPH_DATA.put('O', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0, 2, 4, 2, 0});
+        GLYPH_DATA.put('P', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 2, 4, 2, 2});
+        GLYPH_DATA.put('Q', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 1, 0, 1, 0, 2, 1, 2, 1, 2, 4, 1, 1, 2, 0});
+        GLYPH_DATA.put('R', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 2, 4, 2, 2, 0, 2, 2, 2, 0, 2, 2, 0});
+        GLYPH_DATA.put('S', new int[] {0, 2, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2});
+        GLYPH_DATA.put('T', new int[] {1, 0, 1, 4, 0, 4, 2, 4});
+        GLYPH_DATA.put('U', new int[] {0, 0, 0, 4, 0, 0, 2, 0, 2, 4, 2, 0});
+        GLYPH_DATA.put('V', new int[] {0, 4, 1, 0, 1, 0, 2, 4});
+        GLYPH_DATA.put('W', new int[] {0, 0, 0, 4, 0, 0, 1, 2, 1, 2, 2, 0, 2, 0, 2, 4});
+        GLYPH_DATA.put('X', new int[] {0, 0, 2, 4, 0, 4, 2, 0});
+        GLYPH_DATA.put('Y', new int[] {0, 4, 1, 2, 1, 0, 1, 2, 1, 2, 2, 4});
+        GLYPH_DATA.put('Z', new int[] {0, 4, 2, 4, 0, 0, 2, 4, 0, 0, 2, 0});
 
-    static final private int[][] GLYPH_AZ = {
-      {0, 0, 0, 4, 0, 4, 2, 4, 2, 4, 2, 0, 0, 2, 2, 2}, // A
-      {0, 0, 0, 4, 0, 4, 2, 3, 0, 2, 2, 2, 0, 0, 2, 0, 2, 3, 2, 0}, 
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0}, 
-      {0, 0, 0, 4, 0, 4, 2, 3, 2, 3, 2, 0, 0, 0, 2, 0}, 
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0}, // E
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2}, 
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0, 2, 0, 2, 2, 1, 2, 2, 2}, // G
-      {0, 0, 0, 4, 0, 2, 2, 2, 2, 0, 2, 4}, 
-      {0, 0, 0, 4}, 
-      {0, 0, 1, 0, 1, 0, 1, 4, 0, 4, 2, 4}, // J
-      {0, 0, 0, 4, 0, 2, 2, 3, 0, 2, 2, 0}, 
-      {0, 0, 0, 4, 0, 0, 2, 0}, 
-      {0, 0, 0, 4, 0, 4, 1, 2, 1, 2, 2, 4, 2, 4, 2, 0}, 
-      {0, 0, 0, 4, 0, 4, 2, 0, 2, 4, 2, 0}, 
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0, 2, 4, 2, 0}, // O
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 2, 4, 2, 2, }, 
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 1, 0, 1, 0, 2, 1, 2, 1, 2, 4, 1, 1, 2, 0}, 
-      {0, 0, 0, 4, 0, 4, 2, 4, 2, 4, 2, 2, 0, 2, 2, 2, 0, 2, 2, 0}, 
-      {0, 2, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2}, // S
-      {1, 0, 1, 4, 0, 4, 2, 4}, 
-      {0, 0, 0, 4, 0, 0, 2, 0, 2, 4, 2, 0}, 
-      {0, 4, 1, 0, 1, 0, 2, 4}, 
-      {0, 0, 0, 4, 0, 0, 1, 2, 1, 2, 2, 0, 2, 0, 2, 4}, // W
-      {0, 0, 2, 4, 0, 4, 2, 0}, 
-      {0, 4, 1, 2, 1, 0, 1, 2, 1, 2, 2, 4}, 
-      {0, 4, 2, 4, 0, 0, 2, 4, 0, 0, 2, 0}
-    };
-    static final private int[][] GLYPH_09 = {
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0, 2, 4, 2, 0}, // 0
-      {1, 0, 1, 4, 1, 3, 1, 4}, 
-      {2, 4, 2, 3, 0, 0, 0, 1, 0, 4, 2, 4, 0, 1, 2, 3, 0, 0, 2, 0}, // 2
-      {0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 4, 2, 0}, 
-      {1, 4, 0, 1, 1, 0, 1, 4, 0, 1, 2, 1}, 
-      {0, 2, 1, 4, 1, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2}, // 5
-      {0, 0, 0, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2}, 
-      {0, 4, 2, 4, 0, 0, 2, 4}, 
-      {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 4, 2, 0}, 
-      {0, 2, 2, 2, 0, 4, 2, 4, 0, 2, 0, 4, 2, 4, 2, 0} // 9
-   };
+        // Numbers 0-9
+        GLYPH_DATA.put('0', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 0, 2, 0, 2, 4, 2, 0});
+        GLYPH_DATA.put('1', new int[] {1, 0, 1, 4, 1, 3, 1, 4});
+        GLYPH_DATA.put('2', new int[] {2, 4, 2, 3, 0, 0, 0, 1, 0, 4, 2, 4, 0, 1, 2, 3, 0, 0, 2, 0});
+        GLYPH_DATA.put('3', new int[] {0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 4, 2, 0});
+        GLYPH_DATA.put('4', new int[] {1, 4, 0, 1, 1, 0, 1, 4, 0, 1, 2, 1});
+        GLYPH_DATA.put('5', new int[] {0, 2, 1, 4, 1, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2});
+        GLYPH_DATA.put('6', new int[] {0, 0, 0, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 0, 2, 2});
+        GLYPH_DATA.put('7', new int[] {0, 4, 2, 4, 0, 0, 2, 4});
+        GLYPH_DATA.put('8', new int[] {0, 0, 0, 4, 0, 4, 2, 4, 0, 2, 2, 2, 0, 0, 2, 0, 2, 4, 2, 0});
+        GLYPH_DATA.put('9', new int[] {0, 2, 2, 2, 0, 4, 2, 4, 0, 2, 0, 4, 2, 4, 2, 0});
 
-    /**
-     * Get the glyph array for a character.
-     * @param ch Character to get glyph for
-     * @return Coordinate array for the glyph, or null if character not supported
-     */
-    private static int[] getGlyphCode(char ch) {
-        if (ch >= 'A' && ch <= 'Z') {
-            return GLYPH_AZ[ch - 'A'];
-        } else if (ch >= '0' && ch <= '9') {
-            return GLYPH_09[ch - '0'];
-        } else if (ch == ' ') {
-            return CHAR_SPACE;
-        } else if (ch == '_') {
-            return CHAR_UNDERSCORE;
-        } else if (ch == '+') {
-            return CHAR_PLUS;
-        } else if (ch == '-') {
-            return CHAR_MINUS;
-        } else if (ch == '?') {
-            return CHAR_QUESTION;
-        } else if (ch == '/') {
-            return CHAR_SLASH;
-        } else if (ch == '<') {
-            return CHAR_LESS;
-        } else if (ch == '>') {
-            return CHAR_MORE;
-        } else {
-            return CHAR_ANY;
-        }
+        // Default for unsupported characters
+        GLYPH_DATA.put('\0', new int[] {2, 2, 1, 4, 1, 4, 0, 2, 0, 2, 1, 0, 1, 0, 2, 2});  // Question mark box
     }
 
     private static List<PathDetail> getPathsForChar(char ch, Colour colour) {
-        int[] glyph = getGlyphCode(ch);
+        // Get glyph data, defaulting to the unknown character glyph
+        int[] glyph = GLYPH_DATA.getOrDefault(ch, GLYPH_DATA.get('\0'));
+
         List<PathDetail> glyphPaths = new ArrayList<>();
         for (int i = 0; i < glyph.length; i += 4) {
             Coord2D from = new Coord2D(glyph[i], -glyph[i + 1] + CHAR_HEIGHT);
@@ -172,6 +149,6 @@ public class TextDetailTranslater extends SketchDetailTranslater<TextDetail> {
 
         return pathDetails;
     }
-    
+
 
 }
